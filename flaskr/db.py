9c5c7734faba5926014a0d854495ada1f7c8e610
Ali <Sam>
2021-07-05 00:31:19 +0430

    Defined three functions: init_db, init_db_command and init_app

--- flaskr/db.py
+++ flaskr/db.py
@@ -6,6 +6,12 @@ from flask.cli import with_appcontext
 
 
 def get_db():
+    """
+    returns a database connection, which is used to execute the commands read from the file.
+
+    Returns:
+        [type]: [description]
+    """
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
@@ -21,3 +27,28 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+
+"""
+click.command() defines a command line command called init-db that 
+calls the init_db function and shows a success message to the user. 
+"""
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
