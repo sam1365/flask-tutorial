6d7ee9d7f050e0958b0edf61d032f3910be43f5a
Ali <Sam>
2021-07-08 11:51:03 +0430

    add some comments to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name='flaskr',
     version='1.0.0',
+    # packages tells Python what package directories (and the Python files they contain) 
+    # to include. 
+    # find_packages() finds these directories automatically so you don’t have to type them out.
     packages=find_packages(),
+    # To include other files, such as the static and templates directories, 
+    # include_package_data is set.
     include_package_data=True,
     zip_safe=False,
     install_requires=[
